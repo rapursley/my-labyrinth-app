56b445cbd22ef492d07bdf5af884b047a0f5197d
rapursley <rpursley@gmail.com>
2017-12-31 17:32:32 -0600

    initial code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# my-labyrinth-app
+# org-mylabyrinth-app
+
+MyLabyrinth.org setup
+
+Uses Webpack, Babel and React
+
+This is example repo for webpack/babel/react tutorial:
+
+[https://stanko.github.io/webpack-babel-react-revisited](https://stanko.github.io/webpack-babel-react-revisited)
---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+﻿import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+//import '../css/style.css';
+import '../css/App.css';
+
+
+//import keenImage from '../assets/keen.png';
+
+
+export default class Hello extends Component {
+    render() {
+        return (
+            <div className='App'>
+				<header className='App-header'>
+                <h1 className='App-title'>Welcome to MyLabyrinth.org</h1>
+                    {/*<img src={ keenImage } alt='Commander Keen' /> */}
+				</header>
+				<p className="App-intro">
+          		Coming soon.
+        		</p>
+            </div>
+        );
+    }
+}
+
+render(<Hello />, document.getElementById('app'));
